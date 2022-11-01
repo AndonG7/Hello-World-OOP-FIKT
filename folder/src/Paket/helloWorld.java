3fa7f05b6b397ab6bcbf27665f6abeaa04f970d5
AndonG7 <gjorgjiev.andon@uklo.edu.mk>
2022-11-01 16:13:23 +0100

    commit za domasnata zadaca

---
+++ folder/src/Paket/helloWorld.java
@@ -0,0 +1,9 @@
+package Paket;
+
+public class helloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World OOP FIKT");
+	}
+}
+
+//https://github.com/AndonG7/Hello-World-OOP-FIKT.git
